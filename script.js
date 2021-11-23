ea7209f0d46f98a3c981f4e3cc25cb12665672b6
Millie Cheung <cheungmillie9@gmail.com>
2021-11-23 16:19:56 +0100

    improved header effect and separated css files

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 window.onscroll = function() {
     let header = document.getElementById('header');
-    let headerLinks = header.querySelectorAll('header-links');
+    let headerLinks = header.querySelector('.header-links');
+    console.log(headerLinks)
     let top = window.scrollY;
     if (top >= 115) {
         header.style.background = "rgb(108, 168, 202)";
+        for (const item of headerLinks.children) {
+            item.style.color = "white";
+        }
     } else {
         header.style.background = "";
+        for (const item of headerLinks.children) {
+            item.style.color = "#3c3c3c";
+        }
     }
 
 } 
