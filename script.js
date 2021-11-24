28b7a1fd688cf4d829b9a4288db9dc996ae90cb0
Millie Cheung <cheungmillie9@gmail.com>
2021-11-24 14:04:01 +0100

    added media query (css) for s-m and xl screens

--- script.js
+++ script.js
@@ -2,17 +2,16 @@
 
 window.onscroll = function() {
     let header = document.getElementById('header');
-    let headerLinks = header.querySelector('.header-links')
-    let headerLink = header.querySelectorAll('.header-links')
+    let headerLinks = header.querySelectorAll('.header-links')
     let top = window.scrollY;
     if (top >= 115) {
         header.style.background = "rgb(108, 168, 202)";
-        for (let item of headerLink) {
+        for (let item of headerLinks) {
             item.children[0].style.color = "white";
         }
     } else {
         header.style.background = "";
-        for (let item of headerLink) {
+        for (let item of headerLinks) {
             item.children[0].style.color = "#3c3c3c";
         }
     } 
