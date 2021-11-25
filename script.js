015152f0e01b7f3ea3a94d874d29bc4707df74f0
Millie Cheung <cheungmillie9@gmail.com>
2021-11-25 17:00:32 +0100

    Added 'read more' JS script but only read more works well but not read less

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+window.addEventListener('load', main);
 window.addEventListener('scroll', onScroll);
-window.addEventListener('click', unhideText);
+window.addEventListener('click', unhideText); ///
+
+function main() { ///
+    addEventListeners(); ///
+}
 
 function onScroll() {
     updateHeaderColor();
     updatePlanePosition();
+}
+
+function addEventListeners() { ///
+    ABC(); ///
 }
 
 function updateHeaderColor() {
@@ -32,30 +41,45 @@ function updatePlanePosition() {
     planePic.style.marginLeft = marginPlane + 'px';
 }
 
-// function unhideText() {
-//     let dots = document.querySelectorAll('.dots');
-//     let moreText = document.querySelectorAll('.more2');
-//     let readMoreButton = document.getElementById('readmore1');
-
-//     if (dots.style.display === "none") {
-//         dots.style.display = "inline";
-//         moreText.style.display = "none";
-//     } else {
-//         dots.style.display = "none";
-//         moreText.style.display = "inline"    
-//     }
-// }
 
 
-let noOfCharac = 100;
-let descriptions = document.querySelectorAll('.portfo-description');
-descriptions.forEach(description => {
+//////
+
+function ABC() { 
+    let noOfCharac = 100;
+    let descriptions = document.querySelectorAll('.portfo-description');
+    descriptions.forEach(description => {
     if (description.textContent.length < noOfCharac) {
         description.nextElementSibling.style.display = "none";
     } else {
         let displayText = description.textContent.slice(0,noOfCharac);
         let moreText = description.textContent.slice(noOfCharac);
-        description.innerHTML = "hello";
-    }
-});
+        description.innerHTML = `${displayText}<span id="dots">...</span><span id="moretext">${moreText}</span>`;
+        let textToHide = document.getElementById('moretext');
+        textToHide.classList.add('invisible');
+        }
+    })
+}
 
+
+ function unhideText() {
+    let readBtn = document.getElementById('readmore');
+    let noOfCharac = 100;
+    let textToHide = document.getElementById('moretext');
+    let dots = document.getElementById('dots');
+
+    if (textToHide.textContent.length < noOfCharac + '3') {
+         textToHide.classList.remove('invisible');
+         dots.classList.add('invisible');
+         readBtn.innerHTML = 'Read less <i class="fas fa-angle-up"></i>';
+        
+         console.log('this is 1:' + textToHide);
+         console.log('this is 1:' + dots);
+       } else {
+         textToHide.classList.add('invisible');
+         dots.classList.remove('invisible');
+         readBtn.innerHTML = 'Read more <i class="fas fa-angle-down"></i>'
+         console.log('this is 2:' + textToHide);
+         console.log('this is 2:' + dots);
+       }
+}
